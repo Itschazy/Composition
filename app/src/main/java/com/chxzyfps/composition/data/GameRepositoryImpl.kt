620f151b4e76c0b21c7b9a53a92fd0d3fa9f21bc
Eugeny Chudov <eschudov@gmail.com>
2023-03-13 23:39:24 +0300

    Реализация Data-слоя (GameRepositoryImpl)

---
+++ app/src/main/java/com/chxzyfps/composition/data/GameRepositoryImpl.kt
@@ -0,0 +1,66 @@
+package com.chxzyfps.composition.data
+
+import com.chxzyfps.composition.domain.entity.GameSettings
+import com.chxzyfps.composition.domain.entity.Level
+import com.chxzyfps.composition.domain.entity.Question
+import com.chxzyfps.composition.domain.repository.GameRepository
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
+
+object GameRepositoryImpl : GameRepository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 2
+
+    override fun generateQuestion(maxSum: Int, countOfOptions: Int): Question {
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxSum + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer - countOfOptions, MIN_ANSWER_VALUE)
+        val to = min(maxSum, rightAnswer + countOfOptions)
+        while (options.size < countOfOptions) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Question(sum, visibleNumber, options.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        return when (level) {
+            Level.TEST -> {
+                GameSettings(
+                    10,
+                    3,
+                    50,
+                    8
+                )
+            }
+            Level.EASY -> {
+                GameSettings(
+                    10,
+                    10,
+                    70,
+                    60
+                )
+            }
+            Level.NORMAL -> {
+                GameSettings(
+                    20,
+                    20,
+                    80,
+                    40
+                )
+            }
+            Level.HARD -> {
+                GameSettings(
+                    30,
+                    30,
+                    90,
+                    40
+                )
+            }
+        }
+    }
+}
